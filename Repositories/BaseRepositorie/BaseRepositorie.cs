55718123c2de560ba53a6a43d34425344c238274
M N Tushar <mntushar25@gmail.com>
2022-04-20 16:42:49 +0600

    base repositories

---
+++ Repositories/BaseRepositorie/BaseRepositorie.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models.DbContexts;
+using Repositories.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repositories.BaseRepositorie
+{
+    class BaseRepositorie<T> : IBaseRepositorie<T> where T : class
+    {
+        protected CollegeDbContext _db;
+        private DbSet<T> Table
+        {
+            get { return _db.Set<T>(); }
+        }
+
+        public BaseRepositorie()
+        {
+            _db = new CollegeDbContext();
+        }
+
+        public virtual bool Add(T entity)
+        {
+            Table.Add(entity);
+            return _db.SaveChanges() > 0;
+        }
+
+        public virtual bool Update(T entity)
+        {
+            _db.Entry(entity).State = EntityState.Modified;
+            return _db.SaveChanges() > 0;
+        }
+
+        public virtual bool Remove(T entity)
+        {
+            Table.Remove(entity);
+            return _db.SaveChanges() > 0;
+        }
+
+        public virtual T Get(int? id)
+        {
+            return Table.Find(id);
+        }
+
+        public virtual ICollection<T> GetAll()
+        {
+            return Table.ToList();
+        }
+    }
+}
