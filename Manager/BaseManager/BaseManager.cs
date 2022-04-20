dd5ed1b0aeaba55b9563204810596c60965f339b
M N Tushar <mntushar25@gmail.com>
2022-04-20 16:46:56 +0600

    base banager

---
+++ Manager/BaseManager/BaseManager.cs
@@ -0,0 +1,43 @@
+﻿using Manager.Interface;
+using Repositories.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manager.BaseManager
+{
+    class BaseManager<T> : IBaseManager<T> where T : class
+    {
+        private IBaseRepositorie<T> _baseRepositorie;
+
+        public BaseManager(IBaseRepositorie<T> baseRepositorie)
+        {
+            _baseRepositorie = baseRepositorie;
+        }
+
+        public virtual bool Add(T entity)
+        {
+            return _baseRepositorie.Add(entity);
+        }
+
+        public virtual T Get(int? id)
+        {
+            return _baseRepositorie.Get(id);
+        }
+
+        public virtual ICollection<T> GetAll()
+        {
+            return _baseRepositorie.GetAll();
+        }
+
+        public virtual bool Remove(T entity)
+        {
+            return _baseRepositorie.Remove(entity);
+        }
+
+        public virtual bool Update(T entity)
+        {
+            return _baseRepositorie.Update(entity);
+        }
+    }
+}
