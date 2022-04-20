30468b43ca0df424eea93823ab24ebe8177b8395
M N Tushar <mntushar25@gmail.com>
2022-04-20 16:50:45 +0600

    dept repo

---
+++ Repositories/DepartmentRepositorie.cs
@@ -0,0 +1,13 @@
+﻿using Models;
+using Repositories.BaseRepositorie;
+using Repositories.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repositories
+{
+    class DepartmentRepositorie : BaseRepositorie<DepartmentModel>, IDepartmentRepositorie
+    {
+    }
+}
