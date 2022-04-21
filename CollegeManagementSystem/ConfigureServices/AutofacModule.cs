2b589ab75f10828982f9cedb11e0a32ef0dc7a67
M N Tushar <mntushar25@gmail.com>
2022-04-21 11:08:51 +0600

    config autofact and automap

---
+++ CollegeManagementSystem/ConfigureServices/AutofacModule.cs
@@ -0,0 +1,32 @@
+﻿using Autofac;
+using Manager;
+using Manager.Interface;
+using Repositories;
+using Repositories.Interface;
+
+namespace CollegeManagementSystem.ConfigureServices
+{
+    public class AutofacModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            // The generic ILogger<TCategoryName> service was added to the ServiceCollection by ASP.NET Core.
+            // It was then registered with Autofac using the Populate method. All of this starts
+            // with the `UseServiceProviderFactory(new AutofacServiceProviderFactory())` that happens in Program and registers Autofac
+            // as the service provider.
+            //builder.Register(c => new DepartmentManager(c.Resolve<ILogger<ValuesService>>()))
+
+            builder.RegisterType<DepartmentManager>().As<IDepartmentManager>();
+            builder.RegisterType<DepartmentRepositorie>().As<IDepartmentRepositorie>();
+
+            //builder.RegisterType<StudentManager>().As<IStudentManager>();
+            //builder.RegisterType<StudentRepositore>().As<IStudentRepositore>();
+
+            //builder.RegisterType<CourseManager>().As<ICourseManager>();
+            //builder.RegisterType<CourseRepositore>().As<ICourseRepositorie>();
+
+            //builder.RegisterType<StudentRegristrationManager>().As<IStudentRegristrationManager>();
+            //builder.RegisterType<StudentRegristrationRepositore>().As<IStudentRegristrationRepositorie>();
+        }
+    }
+}
---
+++ CollegeManagementSystem/ConfigureServices/AutomapperProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollegeManagementSystem.ConfigureServices
+{
+    public class AutomapperProfile : Profile
+    {
+        public AutomapperProfile()
+        {
+            //CreateMap<DepartmentDetailsMv, DepartmentModel>();
+            //CreateMap<DepartmentModel, DepartmentDetailsMv>();
+
+            //CreateMap<StudentModel, StudentCreateMv>();
+            //CreateMap<StudentCreateMv, StudentModel>();
+
+            //CreateMap<StudentModel, StudentEditMv>();
+            //CreateMap<StudentEditMv, StudentModel>();
+        }
+    }
+}
