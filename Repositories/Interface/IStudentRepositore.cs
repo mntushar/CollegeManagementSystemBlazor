f2d02ea2e4d33bb1eebb166e20859c1f8cf17478
mntushar <mntushar25@gmail.com>
2022-05-06 23:57:21 +0600

    add student repositories

---
+++ Repositories/Interface/IStudentRepositore.cs
@@ -0,0 +1,12 @@
+﻿using Models;
+using Repositories.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repositories.Interface
+{
+    public interface IStudentRepositore : IBaseRepositorie<StudentModel>
+    {
+    }
+}
