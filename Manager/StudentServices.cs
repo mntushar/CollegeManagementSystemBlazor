83dfb7bd16d5cc0e711fd93f50a3e4a9effa36f8
mntushar <mntushar25@gmail.com>
2022-05-07 00:03:42 +0600

    add student services

---
+++ Manager/StudentServices.cs
@@ -0,0 +1,48 @@
+﻿using Models;
+using Repositories.Interface;
+using Services.BaseManager;
+using Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services
+{
+    public class StudentServices : BaseManager<StudentModel>, IStudentServices
+    {
+        private IStudentRepositore _studentRepositore;
+        private IDepartmentManager _departmentManager;
+
+        public StudentServices(IStudentRepositore studentRepositore) : base(studentRepositore)
+        {
+            _studentRepositore = studentRepositore;
+        }
+
+        public StudentServices(IStudentRepositore studentRepositore, IDepartmentManager departmentManager) : base(studentRepositore)
+        {
+            _studentRepositore = studentRepositore;
+            _departmentManager = departmentManager;
+        }
+
+        public override bool Add(StudentModel entity)
+        {
+            DepartmentModel department = _departmentManager.Get(entity.DeptId);
+            if (department != null)
+            {
+                return _studentRepositore.Add(entity);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override bool Update(StudentModel entity)
+        {
+            DepartmentModel department = _departmentManager.Get(entity.DeptId);
+            if (department == null) return false;
+            return _studentRepositore.Update(entity);
+
+        }
+    }
+}
